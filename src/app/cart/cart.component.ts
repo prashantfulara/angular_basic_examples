9c82fe7f3943e80d1656d0f324cd3f73415277a9
prashantfulara <prashant.fulara@gmail.com>
2023-08-23 08:33:22 +0530

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  constructor(private cartService: CartService) {}
 
+  items = this.cartService.getItems();
 }
